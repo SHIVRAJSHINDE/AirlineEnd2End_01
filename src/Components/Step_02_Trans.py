1f2df111ce55e1e594bd583075155973c879eabc
SHIVRAJ SHINDE <shindeshivraaj@gmail.com>
2026-09-22 09:30:41 +0530

    TraininPPLN

---
+++ src/Components/Step_02_Trans.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import joblib
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+
+
+class trasformation:
+    def __init__(self):
+        pass
+
+    def read_Data(self):
+        X_train = pd.read_csv("E:/1.Eathans/Final Projects/AirlineEnd2End_01/Artifact/Data/CleanedData/X_train.csv")
+        X_test = pd.read_csv("E:/1.Eathans/Final Projects/AirlineEnd2End_01/Artifact/Data/CleanedData/X_test.csv")
+        return X_train, X_test
+    def ColumnTransform(self):
+        numericalCol = ['Total_Stops','DurHrs_Mins','D_Day','D_Month','D_Year','D_Hour','D_Minute','A_Hour','A_Minute','A_Day','A_Month','A_Year']
+        OneHotCol =['Airline','Source','Destination'] 
+
+        OneHotEncoding_01 =  ("One_Hot_Encoder",OneHotEncoder(sparse_output=False, drop="first", handle_unknown="ignore"),OneHotCol,)
+        Scaling = ("Numerical_Coluns", StandardScaler(), numericalCol)
+        transform_Obj = ColumnTransformer(transformers=[OneHotEncoding_01,Scaling]).set_output(transform="pandas")
+        return transform_Obj
+
+    def encoding(self,transform_Obj,X_train,X_test):
+        X_train_trans = transform_Obj.fit_transform(X_train)
+        joblib.dump(transform_Obj,"E:/1.Eathans/Final Projects/Airline_ML_02_pra/Artifact/transFile/Transform.pkl")
+        transFile = joblib.load("E:/1.Eathans/Final Projects/Airline_ML_02_pra/Artifact/transFile/Transform.pkl")
+
+        X_train_trans.to_csv("E:/1.Eathans/Final Projects/AirlineEnd2End_01/Artifact/Data/Trans_data/X_Train_Trans.csv",index=False)
+        X_test_trans = transFile.fit_transform(X_test)
+        X_test_trans.to_csv("E:/1.Eathans/Final Projects/AirlineEnd2End_01/Artifact/Data/Trans_data/X_Test_Trans.csv",index=False)
+
--- src/Pipeline/Step_01_Training_ppln.py
+++ src/Pipeline/Step_01_Training_ppln.py
@@ -1,6 +1,24 @@
 from src.Components.Step_01_Cleaning import cleaning
+from src.Components.Step_02_Trans import trasformation
+from src.Components.Step_03_Training import Training
 
-if __name__ == "__main___":
+if __name__ == "__main__":
     cleaningObj = cleaning()
     df = cleaningObj.readData()
-    print(df)
+    df = cleaningObj.DataCleaning(df)
+    df = cleaningObj.process_duration(df)
+    df = cleaningObj.generate_Date_Time(df)
+    df = cleaningObj.drop_unesseary_Col(df)
+    df = cleaningObj.remove_Outlier(df)
+    X,Y = cleaningObj.split_X_y(df)
+    cleaningObj.Train_Test_Split(X,Y)
+
+    TranObj = trasformation()
+    X_train,X_test = TranObj.read_Data()
+    transform_Obj = TranObj.ColumnTransform()
+    TranObj.encoding(transform_Obj,X_train,X_test)
+
+    TrainObj = Training()
+    X_test,X_train,y_test_true,y_train_true,X_train_trans,X_test_trans = TrainObj.read_Data()
+    TrainObj.ModelTraining(X_train,X_train_trans,y_train_true)
+
